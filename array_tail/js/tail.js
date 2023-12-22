46d5efeeb8cb75d202033c020e59eb1b5053a8d1
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-22 17:08:20 +0100

    added tot elements

---
+++ array_tail/js/tail.js
@@ -0,0 +1,3 @@
+// Chiediamo all'utente il numero massimo di elementi da aggiungere ad un array
+const totElements = parseInt(prompt("Quanti elementi deve contenere l'array?")) 
+console.log(totElements);
