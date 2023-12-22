a32d010ad091daed38e6dc51eaaefabb1d7aabc9
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-22 17:02:41 +0100

    added validation

--- array-lista/js/lista.js
+++ array-lista/js/lista.js
@@ -5,3 +5,25 @@ console.log(fridge);
 // Aggiungo la pesca all'array
 fridge.push('pesca');
 console.log(fridge);
+
+// Verifico se nell'array di frutta c'è il cocomero
+let message = "C'è il cocomero in frigo? ";
+
+let peachIntoFridge = false;
+
+for (let i = 0; i < fridge.length && !peachIntoFridge; i++) {
+
+    //Validazione
+    if (fridge[i] === 'cocomero') {
+        peachIntoFridge = true;
+    }
+
+}
+
+    if (peachIntoFridge) {
+        message += 'Trovato! Devo solo preparare il cocktail.'
+    } else {
+        message += 'Oh no, devo uscire a comprare il cocomero!'
+    }
+
+console.log(message);
