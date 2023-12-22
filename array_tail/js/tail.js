bcd35904a3b73463eddca4463efc697838c7dae4
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-22 17:26:59 +0100

    added array

--- array_tail/js/tail.js
+++ array_tail/js/tail.js
@@ -1,3 +1,6 @@
 // Chiediamo all'utente il numero massimo di elementi da aggiungere ad un array
-const totElements = parseInt(prompt("Quanti elementi deve contenere l'array?")) 
+const totElements = parseInt(prompt("Quanti elementi deve contenere l'array?", 9));
 console.log(totElements);
+
+// Creare l'array
+const element = [];
