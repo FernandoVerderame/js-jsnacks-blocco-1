aeed79d5fb4e7149b0a88d3b57d4335975fc411d
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-22 16:46:24 +0100

    added peach into fridge

--- array-lista/js/lista.js
+++ array-lista/js/lista.js
@@ -1,3 +1,7 @@
 // Creo l'array
 const fridge = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 console.log(fridge);
+
+// Aggiungo la pesca all'array
+fridge.push('pesca');
+console.log(fridge);
